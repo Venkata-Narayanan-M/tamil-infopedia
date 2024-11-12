0a9f531a0346897b052ec9d68192325a5e61ec00
VENKATA NARAYANAN <prakash1107@gmail.com>
2024-11-11 19:39:40 -0500

    Adding Accordion and initial pages and app.tsx

---
+++ apps/web/src/components/Accordion/Accordion.tsx
@@ -0,0 +1,18 @@
+const Accordion = () => {
+  return (
+    <div className="flex-col w-4/12">
+      <div>
+        <p>This is a heading</p>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+          ipsam magnam labore adipisci animi? Et alias porro sit inventore
+          veniam fugiat, quo aperiam! Accusantium fuga, a ea eius vero nesciunt.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Accordion from "../components/Accordion/Accordion";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <h1>Welcome to Tamil Infopedia!</h1>
+      <Accordion />
+    </div>
+  );
+};
+
+export default Home;
